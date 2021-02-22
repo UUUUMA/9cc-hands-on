81bfb2de54ab981af1931947354d353708e94ac4
UUUUMA <uma.ryo.prog.acc@gmail.com>
2021-02-23 03:18:40 +0900

    [update] add +, - operators

--- src/main.c
+++ src/main.c
@@ -7,9 +7,29 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
+    char* p = argv[1];
+
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov $%d, %%rax\n", atoi(argv[1]));
+    printf("  mov $%ld, %%rax\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add $%ld, %%rax\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("  sub $%ld, %%rax\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return EXIT_FAILURE;
+    }
+
     printf("  ret\n");
 
     return EXIT_SUCCESS;
