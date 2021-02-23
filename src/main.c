aff9e3553a2408b18cc701d8f6fee61b7f3c623e
UUUUMA <uma.ryo.prog.acc@gmail.com>
2021-02-24 00:23:23 +0900

    [update] improve error message

--- src/main.c
+++ src/main.c
@@ -1,5 +1,116 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    TK_PUNCT,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+    TokenKind kind;
+    Token* next;
+    int val;
+    char* loc;
+    int len;
+};
+
+static void error(char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+static char* current_input;
+static void verror_at(char* loc, char* fmt, va_list ap) {
+    int pos = loc - current_input;
+    fprintf(stderr, "%s\n", current_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+static void error_at(char* loc, char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(loc, fmt, ap);
+}
+
+static void error_tok(Token* tok, char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(tok->loc, fmt, ap);
+}
+
+static bool equal(Token* tok, char* op) {
+    return memcmp(tok->loc, op, tok->len) == 0 && op[tok->len] == '\0';
+}
+
+static Token* skip(Token* tok, char* s) {
+    if (!equal(tok, s)) {
+        error_tok(tok, "expected '%s'", s);
+    }
+    return tok->next;
+}
+
+static int get_number(Token* tok) {
+    if (tok->kind != TK_NUM) {
+        error_tok(tok, "expected a number");
+    }
+    return tok->val;
+}
+
+static Token* new_token(TokenKind kind, char* start, char* end) {
+    Token* tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->loc = start;
+    tok->len = end - start;
+    return tok;
+}
+
+static Token* tokenize() {
+    char* p = current_input;
+    Token head = {};
+    Token* cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur->next = new_token(TK_NUM, p, p);
+            cur = cur->next;
+
+            char* q = p;
+            cur->val = strtol(p, &p, 10);
+            cur->len = q - p;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur->next = new_token(TK_PUNCT, p, p+1);
+            cur = cur->next;
+            p++;
+            continue;
+        }
+
+        error_at(p, "invalid token");
+    }
+
+    cur->next = new_token(TK_EOF, p, p);
+    return head.next;
+}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -7,29 +118,26 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    char* p = argv[1];
+    current_input = argv[1];
+    Token* tok = tokenize(argv[1]);
 
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov $%ld, %%rax\n", strtol(p, &p, 10));
 
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            printf("  add $%ld, %%rax\n", strtol(p, &p, 10));
+    printf("  mov $%d, %%rax\n", get_number(tok));
+    tok = tok->next;
+
+    while (tok->kind != TK_EOF) {
+        if (equal(tok, "+")) {
+            printf("  add $%d, %%rax\n", get_number(tok->next));
+            tok = tok->next->next;
             continue;
         }
-
-        if (*p == '-') {
-            p++;
-            printf("  sub $%ld, %%rax\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "unexpected character: '%c'\n", *p);
-        return EXIT_FAILURE;
+        
+        tok = skip(tok, "-");
+        printf("  sub $%d, %%rax\n", get_number(tok));
+        tok = tok->next;
     }
-
     printf("  ret\n");
 
     return EXIT_SUCCESS;
