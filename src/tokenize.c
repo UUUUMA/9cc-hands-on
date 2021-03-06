6c3480969c6e3fb96f1a6a1bc368f542c8644d64
UUUUMA <uma.ryo.prog.acc@gmail.com>
2021-03-07 00:29:04 +0900

    [update] split main.c into multiple small files

---
+++ src/tokenize.c
@@ -0,0 +1,99 @@
+#include "9cc.h"
+
+void error(char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+static char* current_input;
+static void verror_at(char* loc, char* fmt, va_list ap) {
+    int pos = loc - current_input;
+    fprintf(stderr, "%s\n", current_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+void error_at(char* loc, char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(loc, fmt, ap);
+}
+
+void error_tok(Token* tok, char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(tok->loc, fmt, ap);
+}
+
+bool equal(Token* tok, char* op) {
+    return memcmp(tok->loc, op, tok->len) == 0 && op[tok->len] == '\0';
+}
+
+Token* skip(Token* tok, char* s) {
+    if (!equal(tok, s)) {
+        error_tok(tok, "expected '%s'", s);
+    }
+    return tok->next;
+}
+
+static Token* new_token(TokenKind kind, char* start, char* end) {
+    Token* tok = calloc(1, sizeof(Token));
+    tok->kind  = kind;
+    tok->loc   = start;
+    tok->len   = end - start;
+    return tok;
+}
+
+bool startswith(char* p, char* q) {
+    return strncmp(p, q, strlen(q)) == 0;
+}
+
+static int read_punct(char* p) {
+    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") || startswith(p, ">=")) {
+        return 2;
+    }
+
+    return ispunct(*p) ? 1 : 0;
+}
+
+Token* tokenize(char* p) {
+    current_input = p;
+    Token head    = {};
+    Token* cur    = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur->next = new_token(TK_NUM, p, p);
+            cur       = cur->next;
+
+            char* q  = p;
+            cur->val = strtol(p, &p, 10);
+            cur->len = q - p;
+            continue;
+        }
+
+        int punct_len = read_punct(p);
+        if (punct_len) {
+            cur->next = new_token(TK_PUNCT, p, p + punct_len);
+            cur       = cur->next;
+            p += cur->len;
+            continue;
+        }
+
+        error_at(p, "invalid token");
+    }
+
+    cur->next = new_token(TK_EOF, p, p);
+    return head.next;
+}
